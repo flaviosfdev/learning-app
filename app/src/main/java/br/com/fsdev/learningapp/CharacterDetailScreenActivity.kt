a642e3a19183f62df73847db218832603641be2d
flaviosfdev <flaviosf.dev@gmail.com>
2022-05-27 15:04:11 -0300

    feat: send character to character detail screen

--- app/src/main/java/br/com/fsdev/learningapp/CharacterDetailScreenActivity.kt
+++ app/src/main/java/br/com/fsdev/learningapp/CharacterDetailScreenActivity.kt
@@ -2,10 +2,35 @@ package br.com.fsdev.learningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fsdev.learningapp.databinding.ActivityCharacterDetailScreenBinding
 
 class CharacterDetailScreenActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityCharacterDetailScreenBinding.inflate(
+            layoutInflater
+        )
+    }
+    private val character by lazy {
+        intent?.extras?.getSerializable(CHARACTER) as Character
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_character_detail_screen)
+        setContentView(binding.root)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        with(binding) {
+            characterDetailName.text = character.name
+            characterDetailStatus.text = character.status
+            characterDetailSpecies.text = character.species
+            characterDetailOrigin.text = character.origin
+        }
+    }
+
+    companion object {
+        const val CHARACTER = "character"
     }
 }
