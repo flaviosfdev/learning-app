57baa418a16e2260f83d06ad90e4c4d53e3c02f9
flaviosfdev <flaviosf.dev@gmail.com>
2022-05-27 10:41:52 -0300

    feat: add home launcher and navigate to character detail

--- app/src/main/java/br/com/fsdev/learningapp/HomeActivity.kt
+++ app/src/main/java/br/com/fsdev/learningapp/HomeActivity.kt
@@ -1,11 +1,21 @@
 package br.com.fsdev.learningapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fsdev.learningapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
+
+        binding.homeCharacterButton.setOnClickListener {
+            startActivity(Intent(this, CharacterDetailScreenActivity::class.java))
+        }
+
     }
 }
