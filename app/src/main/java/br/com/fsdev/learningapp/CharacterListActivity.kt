5b70d77bca65e6e0e1b6f0c47661788d8a17c800
flaviosfdev <flaviosf.dev@gmail.com>
2022-05-27 12:51:24 -0300

    feat: add character list screen

---
+++ app/src/main/java/br/com/fsdev/learningapp/CharacterListActivity.kt
@@ -0,0 +1,11 @@
+package br.com.fsdev.learningapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_list)
+    }
+}
