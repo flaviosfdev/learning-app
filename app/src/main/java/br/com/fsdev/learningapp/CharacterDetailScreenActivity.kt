e397ff5b042c0475c1cac6bdf9478064eabff3ce
flaviosfdev <flaviosf.dev@gmail.com>
2022-05-27 10:10:02 -0300

    feat: add character detail screen

---
+++ app/src/main/java/br/com/fsdev/learningapp/CharacterDetailScreenActivity.kt
@@ -0,0 +1,11 @@
+package br.com.fsdev.learningapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterDetailScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_detail_screen)
+    }
+}
