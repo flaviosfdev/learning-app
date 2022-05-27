e5aa472c329657210723ae314bba7c11bfced86d
Flávio Ferreira <flaviosf.dev@gmail.com>
2022-05-27 12:01:06 -0300

    feat: home screen (#2)
    * feat: add home screen
    
    * chore: adjust button style
    
    * chore: add viewBinding
    
    * feat: add home launcher and navigate to character detail
    
    * feat: add home screen and ajust character detail screen

---
+++ app/src/main/java/br/com/fsdev/learningapp/HomeActivity.kt
@@ -0,0 +1,27 @@
+package br.com.fsdev.learningapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fsdev.learningapp.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setup()
+
+    }
+
+    private fun setup() {
+        with(binding) {
+            homeCharacterButton.setOnClickListener {
+                startActivity(Intent(this@HomeActivity, CharacterDetailScreenActivity::class.java))
+            }
+        }
+    }
+}
